fcf14f5f90cbae988e87e8efb561c2d74053f998
Nandhakumar <nandhakumararunachalam2003@gmail.com>
2025-07-09 12:53:00 +0530

    deleted

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app =  express();
-const taskRoute = require('./task/task');
-app.use('/task' ,taskRoute );
-app.listen(3000 , function(){
-    process.stdout.write('server is running');
-});
