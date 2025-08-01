a4ad6933c15e3ccf59c223108e33386e9089f08a
Nandhakumar <nandhakumararunachalam2003@gmail.com>
2025-08-01 17:37:32 +0530

    modify : modified server confiu

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 
@@ -6,12 +5,13 @@ const port = 3000 ;
 const host = '127.0.0.1';
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
 // for task
 const taskRoute = require('./task/task');
+const webRoute = require('./web/webRoute');
 app.use('/task' , taskRoute);
+app.use('/web' , webRoute);
 
-
-app.listen(port  , function(){
+app.listen(port ,host, function(){
     console.log(`server is running on port ${port}`);
 });
